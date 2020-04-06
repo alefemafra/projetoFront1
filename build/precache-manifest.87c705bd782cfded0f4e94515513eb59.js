7ea6aed57128fee4bbf5862b748cc3440d55ea05
Alefe Mafra <emanoelmafra@gmail.com>
2020-04-06 13:26:29 -0300

    init

---
+++ build/precache-manifest.87c705bd782cfded0f4e94515513eb59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f0f0cc50c4f840e831f7f6f4a6eaae9",
+    "url": "/projetoFront1/index.html"
+  },
+  {
+    "revision": "836330337a650bbc8807",
+    "url": "/projetoFront1/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "836330337a650bbc8807",
+    "url": "/projetoFront1/static/js/2.89c2309a.chunk.js"
+  },
+  {
+    "revision": "283e247fcd70152d45e39e6e5a17cbed",
+    "url": "/projetoFront1/static/js/2.89c2309a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6727993b833b15a8330",
+    "url": "/projetoFront1/static/js/main.289e0a91.chunk.js"
+  },
+  {
+    "revision": "64e26806043cf0581b34",
+    "url": "/projetoFront1/static/js/runtime-main.43f71da2.js"
+  },
+  {
+    "revision": "239fb8b6f6d1bf205e93c9a6d28444fb",
+    "url": "/projetoFront1/static/media/logo.239fb8b6.png"
+  }
+]);
